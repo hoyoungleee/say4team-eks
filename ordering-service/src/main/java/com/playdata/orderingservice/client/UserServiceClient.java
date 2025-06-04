a062d57f173a05ac99a3ca9adcf5d784be36ab2d
hoyeong-i <ghdud939@gmail.com>
2025-06-04 14:18:44 +0900

    각 서비스들의 bootstrap.yml 수정

--- ordering-service/src/main/java/com/playdata/orderingservice/client/UserServiceClient.java
+++ ordering-service/src/main/java/com/playdata/orderingservice/client/UserServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "http://user-service.default.svc.cluster.local:8081")
 public interface UserServiceClient {
 
     @GetMapping("/user/findByEmail")
